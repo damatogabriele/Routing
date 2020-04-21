6c5d0686ded54ac2cc5aab4ee6ecda3ac9c92bc3
damatogabriele <damato.gabriele@iisgalvanimi.edu.it>
2020-04-21 17:28:34 +0000

    art

---
+++ first-routed-app/src/app/spotify.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+
+//Dichiaro che il servizio è iniettabile agli altri componenti a partire dal componente root
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+  //url per oauth: https://developer.spotify.com/console/get-search-item/
+  //Ottengo il modulo HttpClient
+  constructor(private http: HttpClient) { }
+
+  searchTrack(query: string) {
+    const url = `https://api.spotify.com/v1/search?q=${query}&type=track`;
+    const headers = new HttpHeaders({
+      Authorization:
+        'Bearer BQCVcy7wm28tyMC74FfhcRPtRpNjZfnIk0eFb3lQRPwXwlBBey4FBAcHq6OXy3kSTXOa3zf_xafgFxKIZtB7irOdv0hxFNP8nyqt7stkkGBZoLooK1nwMBUxeL0gyAqMl2s46CM9bkPWpZM'
+    });
+
+    let obsTracks = this.http.get(url, { headers });
+    return obsTracks;
+    //Ritorno un observable ai componenti che richiedono il servizio
+  }
+
+  getTrack(id: string) {
+    const url = `https://api.spotify.com/v1/tracks/${id}`;
+    const headers = new HttpHeaders({
+      Authorization: environment.oauthToken
+    });
+    return this.http.get(url, { headers });
+  }
+
+
+}
---
+++ first-routed-app/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+production: false,
+oauthToken: "Bearer BQCVcy7wm28tyMC74FfhcRPtRpNjZfnIk0eFb3lQRPwXwlBBey4FBAcHq6OXy3kSTXOa3zf_xafgFxKIZtB7irOdv0hxFNP8nyqt7stkkGBZoLooK1nwMBUxeL0gyAqMl2s46CM9bkPWpZM"
+};
